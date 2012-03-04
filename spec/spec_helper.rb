ec2f18a0d2ec406c9f1d4f692da28150ea0d48ae
Chris Boertien <chris.boertien@bz-labs.com>
2012-03-03 19:22:18 -0700

    setup devise controller testing support
    Signed-off-by: Chris Boertien <chris.boertien@bz-labs.com>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,5 +8,7 @@ RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = true
   config.infer_base_class_for_anonymous_controllers = false
+  config.include Devise::TestHelpers, :type => :controller
+  config.extend ControllerMacros, :type => :controller
 end
 
