3373249bc7f8bdf507f93122b630b5a9946da67e
Chris Boertien <chris.boertien@bz-labs.com>
2012-03-04 18:31:58 -0700

    add specs for due_at select and links
    Signed-off-by: Chris Boertien <chris.boertien@bz-labs.com>

--- spec/views/tasks/edit.html.erb_spec.rb
+++ spec/views/tasks/edit.html.erb_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "tasks/edit" do
   before(:each) do
+    @user = stub_model(User)
+    @controller.stub(:current_user => @user)
     @task = assign(:task, stub_model(Task,
       :subject => "MyString",
       :description => "MyText",
@@ -16,6 +18,21 @@ describe "tasks/edit" do
     assert_select "form", :action => tasks_path(@task), :method => "post" do
       assert_select "input#task_subject", :name => "task[subject]"
       assert_select "textarea#task_description", :name => "task[description]"
+      assert_select "select#task_due_at_1i", :name => "task[due_at(1i)]"
+      assert_select "select#task_due_at_2i", :name => "task[due_at(2i)]"
+      assert_select "select#task_due_at_3i", :name => "task[due_at(3i)]"
+      assert_select "select#task_due_at_4i", :name => "task[due_at(4i)]"
+      assert_select "select#task_due_at_5i", :name => "task[due_at(5i)]"
     end
+  end
+
+  it "has a link to show the task" do
+    render
+    rendered.should have_selector("a[href='/tasks/#{@task.to_param}']")
+  end
+
+  it "has a link to go back to the users show action" do
+    render
+    rendered.should have_selector("a[href='/users/#{@user.to_param}']")
   end
 end
