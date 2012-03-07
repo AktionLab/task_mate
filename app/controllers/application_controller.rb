c66d82e9152f2c861869c0e9a49e69421c74ec75
Chris Boertien <chris.boertien@bz-labs.com>
2012-03-07 10:01:34 -0700

    rescue from access denied with redirects to root or sign in
    Signed-off-by: Chris Boertien <chris.boertien@bz-labs.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  if Rails.env.production? or Rails.env.development?
+    rescue_from CanCan::AccessDenied do
+      if current_user
+        redirect_to root_path, :alert => "You are not authorized to view that page."
+      else
+        redirect_to new_user_session_path, :alert => "Please login to view the page."
+      end
+    end
+  end
 end
