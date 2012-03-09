006f3f6191660426662bc311871235e7987b2d60
Chris Boertien <chris.boertien@bz-labs.com>
2012-03-09 00:00:21 -0700

    add state transitions
    Signed-off-by: Chris Boertien <chris.boertien@bz-labs.com>

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,8 +1,54 @@
 class Task < ActiveRecord::Base
   attr_accessible :subject, :description, :due_at
 
+  before_create :set_default_state
+
   has_many :assignments, :dependent => :delete_all
 
   validates :subject, :presence => true, :length => {:maximum => 255} 
   validates :description, :length => {:maximum => 1000}
+
+  def new?
+    state == 'new'
+  end
+
+  def cancelled?
+    state == 'cancelled'
+  end
+
+  def completed?
+    state == 'completed'
+  end
+
+  def expired?
+    state == 'expired'
+  end
+
+  def complete!
+    raise "Cannot complete cancelled task" if cancelled?
+    raise "Cannot complete expired task" if expired?
+    self.completed_at = DateTime.now
+    self.state = 'completed'
+    self.save
+  end
+
+  def cancel!
+    raise "Cannot cancel completed task" if completed?
+    raise "Cannot cancel expired task" if expired?
+    self.state = 'cancelled'
+    self.save
+  end
+
+  def expire!
+    raise "Cannot exire completed task" if completed?
+    raise "Cannot expire cancelled task" if cancelled?
+    self.state = 'expired'
+    self.save
+  end
+
+private
+  
+  def set_default_state
+    self.state = 'new'
+  end
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe Task do
+  let(:task) { Factory(:task) }
+
   describe 'factories' do
     it 'should have a valid default factory' do
       Factory.build(:task).should be_valid
@@ -25,6 +27,94 @@ describe Task do
     it 'should remove all assignments when deleted' do
       assignment = Factory(:assignment)
       expect { assignment.task.destroy }.to change { Assignment.count }.by(-1)
+    end
+  end
+
+  describe 'state' do
+    it 'by default is new' do
+      task.state.should == 'new'
+    end   
+
+    it 'should return true for new? when state is new' do
+      task.should be_new
+    end
+
+    it 'should return true for completed? when state is completed' do
+      task.complete!
+      task.reload
+      task.should be_completed
+    end
+
+    it 'should return true for cancelled? when state is cancelled' do
+      task.cancel!
+      task.reload
+      task.should be_cancelled
+    end
+
+    it 'transitions from new to completed' do
+      task.complete!
+      task.reload
+      task.state.should == 'completed'
+    end
+
+    it 'transitions from new to cancelled' do
+      task.cancel!
+      task.reload
+      task.state.should == 'cancelled'
+    end
+
+    it 'transitions from new to expired' do
+      task.expire!
+      task.reload
+      task.state.should == 'expired'
+    end
+
+    it 'should not allow transition from completed to cancelled' do
+      task.complete!
+      expect {
+        task.cancel!
+      }.to raise_error(RuntimeError)
+    end
+
+    it 'should not allow transition from completed to expired' do
+      task.complete!
+      expect {
+        task.expire!
+      }.to raise_error(RuntimeError)
+    end
+
+    it 'should not allow transition from cancelled to completed' do
+      task.cancel!
+      expect {
+        task.complete!
+      }.to raise_error(RuntimeError)
+    end
+
+    it 'should not allow transition from cancelled to expired' do
+      task.cancel!
+      expect {
+        task.expire!
+      }.to raise_error(RuntimeError)
+    end
+
+    it 'should not allow transition from expired to completed' do
+      task.expire!
+      expect {
+        task.complete!
+      }.to raise_error(RuntimeError)
+    end
+
+    it 'should not allow transition from expired ot cancelled' do
+      task.expire!
+      expect {
+        task.cancel!
+      }.to raise_error(RuntimeError)
+    end
+
+    it 'should set the completed at time when completed' do
+      task.complete!
+      task.reload
+      task.completed_at.should_not be_nil
     end
   end
 end
