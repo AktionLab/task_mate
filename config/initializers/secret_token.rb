19db06f3eb488b0aebb87a070a56a948350fcf4d
Chris Boertien <chris.boertien@bz-labs.com>
2012-03-09 00:19:45 -0700

    mod some configs to fix login issues
    Signed-off-by: Chris Boertien <chris.boertien@bz-labs.com>

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-TaskMate::Application.config.secret_token = '692737121e31f0d703e96cdd92e5fceef2301ede93eae8744db0c67da67122405ab9c7c918688b9b15380547052b35f5fcad5da2657ff525f365e416ee9691dc'
+TaskMate::Application.config.secret_token = '692737121e31f0d703e96cdd92e5fceef2301ede93eae8744db0c67da67122395ab9c7c918688b9b15380547052b35f5fcad5da2657ff525f365e416ee9691dc'
