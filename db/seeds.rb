cc759912894ba68a14a46651179a68d8647f3c4e
Chris Boertien <chris.boertien@bz-labs.com>
2012-03-18 17:17:12 -0600

    add tasks in different states and remove extra users
    Signed-off-by: Chris Boertien <chris.boertien@bz-labs.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,23 @@
 Task.delete_all
 User.delete_all
 Assignment.delete_all
+TaskList.delete_all
 
 @user = User.create! :first_name => "Test", :last_name => "Seed", :alias_name => "Tester", :email => "test@example.com", :password => 'password', :password_confirmation => 'password'
 
-3.times do |n|
-  User.create!(:first_name => "Test", :last_name => "Seed", :alias_name => "Tester#{n}", :email => "test-#{n}@example.com", :password => 'password', :password_confirmation => 'password').tap do |user|
-    user.tasks.create! :subject => "Foo", :description => "Bar"
-  end
+5.times do |n|
+  @user.tasks.create! :subject => Faker::Lorem.sentence(7), :description => Faker::Lorem.paragraph(3)
+end
+
+5.times do |n|
+  @user.tasks.create!(:subject => Faker::Lorem.sentence(7), :description => Faker::Lorem.paragraph(3)).tap {|task| task.complete!}
+end
+
+5.times do |n|
+  @user.tasks.create!(:subject => Faker::Lorem.sentence(7), :description => Faker::Lorem.paragraph(3)).tap {|task| task.cancel!}
+end
+
+5.times do |n|
+  @user.tasks.create!(:subject => Faker::Lorem.sentence(7), :description => Faker::Lorem.paragraph(3)).tap {|task| task.expire!}
 end
 
