1f26ea5c2259162e053c1106d269134bcb444da1
Chris Boertien <chris.boertien@bz-labs.com>
2012-03-18 17:05:55 -0600

    allow scoping of tasks on params
    Signed-off-by: Chris Boertien <chris.boertien@bz-labs.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,27 @@
 class UsersController < ApplicationController
+  load_and_authorize_resource
+  before_filter :set_scope
 
   # GET /users/1
   # GET /users/1.json
   def show
-    @user = User.find params[:id]
-    authorize! :show, @user
-    @tasks = @user.tasks
+    @tasks = @user.tasks.send(@scope)
 
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @user }
     end
+  end
+
+private
+  
+  def set_scope
+    @scope = 'current' and return if params[:scope].nil? or not valid_scope(params[:scope])
+    @scope = params[:scope]
+  end
+
+  def valid_scope(scope)
+    %w(current completed cancelled expired).include? scope
   end
 end
 
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -15,6 +15,26 @@ describe UsersController do
         get :show, {:id => @user.to_param}
         assigns(:tasks).should eq(tasks)
       end
+
+      describe 'scope' do
+        it 'should default to the current scope' do
+          get :show, {:id => @user.to_param}
+          assigns(:scope).should == 'current'
+        end
+
+        it 'should allow a valid scope to be set' do
+          valid_scopes = %w(current completed cancelled expired)
+          valid_scopes.each do |scope|
+            get :show, {:id => @user.to_param, :scope => scope }
+            assigns(:scope).should == scope
+          end
+        end
+
+        it 'should fallback to the current scope when the scope is invalid' do
+          get :show, {:id => @user.to_param, :scope => 'test_scope'}
+          assigns(:scope).should == 'current'
+        end
+      end
     end
 
     describe "for other users" do
