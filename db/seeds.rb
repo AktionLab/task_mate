b300bbcf639188a61c47a36069f24048b83ffa44
Chris Boertien <chris.boertien@bz-labs.com>
2012-03-19 04:00:10 -0600

    setup tasks under personal task lists and setup some other task lists
    Signed-off-by: Chris Boertien <chris.boertien@bz-labs.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,18 +6,38 @@ TaskList.delete_all
 @user = User.create! :first_name => "Test", :last_name => "Seed", :alias_name => "Tester", :email => "test@example.com", :password => 'password', :password_confirmation => 'password'
 
 5.times do |n|
-  @user.tasks.create! :subject => Faker::Lorem.sentence(7), :description => Faker::Lorem.paragraph(3)
+  @user.personal_task_list.tasks.create! :subject => Faker::Lorem.sentence(7), :description => Faker::Lorem.paragraph(3)
 end
 
 5.times do |n|
-  @user.tasks.create!(:subject => Faker::Lorem.sentence(7), :description => Faker::Lorem.paragraph(3)).tap {|task| task.complete!}
+  @user.personal_task_list.tasks.create!(:subject => Faker::Lorem.sentence(7), :description => Faker::Lorem.paragraph(3)).tap {|task| task.complete!}
 end
 
 5.times do |n|
-  @user.tasks.create!(:subject => Faker::Lorem.sentence(7), :description => Faker::Lorem.paragraph(3)).tap {|task| task.cancel!}
+  @user.personal_task_list.tasks.create!(:subject => Faker::Lorem.sentence(7), :description => Faker::Lorem.paragraph(3)).tap {|task| task.cancel!}
 end
 
 5.times do |n|
-  @user.tasks.create!(:subject => Faker::Lorem.sentence(7), :description => Faker::Lorem.paragraph(3)).tap {|task| task.expire!}
+  @user.personal_task_list.tasks.create!(:subject => Faker::Lorem.sentence(7), :description => Faker::Lorem.paragraph(3)).tap {|task| task.expire!}
+end
+
+3.times do |n|
+  @user.task_lists.create!(:name => "Task List #{n+1}").tap do |task_list|
+    5.times do |n|
+      @user.personal_task_list.tasks.create! :subject => Faker::Lorem.sentence(7), :description => Faker::Lorem.paragraph(3)
+    end
+
+    5.times do |n|
+      @user.personal_task_list.tasks.create!(:subject => Faker::Lorem.sentence(7), :description => Faker::Lorem.paragraph(3)).tap {|task| task.complete!}
+    end
+
+    5.times do |n|
+      @user.personal_task_list.tasks.create!(:subject => Faker::Lorem.sentence(7), :description => Faker::Lorem.paragraph(3)).tap {|task| task.cancel!}
+    end
+
+    5.times do |n|
+      @user.personal_task_list.tasks.create!(:subject => Faker::Lorem.sentence(7), :description => Faker::Lorem.paragraph(3)).tap {|task| task.expire!}
+    end
+  end
 end
 
